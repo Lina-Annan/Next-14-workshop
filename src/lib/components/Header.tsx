00aa59ddfb6f1e11c7d695cf89f1064536608e68
genzzo <alikanso.504@gmail.com>
2024-08-24 17:00:14 +0300

    feat: added header

--- src/lib/components/Header.tsx
+++ src/lib/components/Header.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import SearchInput from "./SearchInput";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { cn } from "../functions/cn";
 
 type Props = {
   handleSetSearchText: (text: string) => void;
@@ -27,14 +32,53 @@ export default function Header({ handleSetSearchText }: Props) {
 
   return (
     <header
-      className={`sticky top-0 left-0 right-0 flex bg-background py-4 mt-6 mb-2 justify-center duration-150 rounded-b-lg z-10 ${
+      className={`sticky top-0 left-0 right-0 flex bg-background py-4 px-20 mt-6 mb-2 justify-between duration-150 rounded-b-lg z-10 ${
         isScrolled && "shadow-lg"
       }`}
     >
+      <div className="w-52" />
       <SearchInput
         placeholder="Search anything"
         onChange={(e) => handleSetSearchText(e.target.value)}
       />
+      <Navigation />
     </header>
+  );
+}
+
+function Navigation() {
+  const pathName = usePathname();
+  const currentLevel = pathName.split("/")[1].split("-")[1];
+
+  const prevLevel = parseInt(currentLevel) - 1;
+  const nextLevel = parseInt(currentLevel) + 1;
+
+  const prevLevelPath = prevLevel >= 0 ? `/level-${prevLevel}` : "";
+  const nextLevelPath = nextLevel <= 6 ? `/level-${nextLevel}` : "";
+
+  return (
+    <nav className="flex items-center gap-10">
+      <div className="flex gap-5 items-center">
+        <Link
+          href={prevLevelPath}
+          className={cn(!prevLevelPath && "opacity-50 cursor-not-allowed")}
+        >
+          {"<"} Previous
+        </Link>
+        <Link
+          href={nextLevelPath}
+          className={cn(!nextLevelPath && "opacity-50 cursor-not-allowed")}
+        >
+          Next {">"}
+        </Link>
+      </div>
+
+      <Link
+        href="/new-post"
+        className="bg-green-600 p-2 rounded-full text-white"
+      >
+        + Create
+      </Link>
+    </nav>
   );
 }
